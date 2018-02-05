28a5ed6f4ae0c8bd1bfb379ae942ca8c971e76b6
Pjott <piotr.baa@gmail.com>
2018-02-05 21:24:28 +0100

    Init button functions

--- app/src/main/java/com/android/pjott/stopwatchappandroid/StopwatchActivity.java
+++ app/src/main/java/com/android/pjott/stopwatchappandroid/StopwatchActivity.java
@@ -2,12 +2,29 @@ package com.android.pjott.stopwatchappandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StopwatchActivity extends Activity {
+
+    private int seconds= 0;
+    private boolean isRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View view) {
+        isRunning = true;
+    }
+
+    public void onClickStop(View view) {
+        isRunning = false;
+    }
+
+    public void onClickReset(View view) {
+        isRunning = false;
+        seconds = 0;
     }
 }
